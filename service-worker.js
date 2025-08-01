c7822333916554bc1419352e0fd84e94cae01aca
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-08-01 12:57:46 +0430

    commit changes

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,3 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   console.log("Service Worker activated.");
 });
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js").then(() => {
-    console.log("Service Worker Registered!");
-  });
-}
--- src/script.js
+++ src/script.js
@@ -4,17 +4,20 @@ function dispalyValue() {
 }
 
 let count = 0;
+
 document.getElementById("add-subject").addEventListener("click", function () {
   count++;
   document.getElementById("count").textContent = ` ${count}`;
 });
+
 document.getElementById("reset-subject").addEventListener("click", function () {
   count = 0;
   document.getElementById("count").textContent = count;
-
-  document.getElementById("input").value = " ";
-  document.getElementById("message").textContent = " ";
+  document.getElementById("input").value = "";
+  document.getElementById("message").textContent = "";
 });
+
+// Register the service worker
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js").then(() => {
     console.log("Service Worker Registered!");
